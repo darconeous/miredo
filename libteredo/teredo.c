0ed378a451cde758a70bf97e234685e0b6051b8e
Rémi Denis-Courmont <rdenis@simphalempin.com>
2005-07-14 18:15:39 +0000

    Forgot this file
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@642 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/teredo.c
+++ libteredo/teredo.c
@@ -32,6 +32,14 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netinet/ip6.h>
+
+#include <fcntl.h>
+#include <sys/socket.h>
+
+#ifndef SOL_IP
+# define SOL_IP IPPROTO_IP
+#endif
+
 #include <libteredo/teredo.h>
 
 /*
@@ -44,3 +52,63 @@ const struct in6_addr teredo_cone =
 	{ { { 0xfe, 0x80, 0, 0, 0, 0, 0, 0,
 		    0x80, 0, 'T', 'E', 'R', 'E', 'D', 'O' } } };
 
+/*
+ * Opens a Teredo UDP/IPv4 socket.
+ */
+int teredo_socket (uint32_t bind_ip, uint16_t port)
+{
+	struct sockaddr_in myaddr = { };
+	int fd, flags;
+
+	myaddr.sin_family = AF_INET;
+	myaddr.sin_port = port;
+	myaddr.sin_addr.s_addr = bind_ip;
+#ifdef HAVE_SA_LEN
+	myaddr.sin_len = sizeof (myaddr);
+#endif
+
+	fd = socket (AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (fd == -1)
+		return -1; // failure
+
+	flags = fcntl (fd, F_GETFL, 0);
+	if (flags != -1)
+		fcntl (fd, F_SETFL, O_NONBLOCK | flags);
+
+	if (bind (fd, (struct sockaddr *)&myaddr, sizeof (myaddr)))
+		return -1;
+
+	flags = 1;
+	setsockopt (fd, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof (flags));
+#ifdef IP_PMTUDISC_DONT
+	/* 
+	 * This tells the (Linux) kernel not to set the Don't Fragment flags
+	 * on UDP packets we send. This is recommended by the Teredo
+	 * specifiation.
+	 */
+	flags = IP_PMTUDISC_DONT;
+	setsockopt (fd, SOL_IP, IP_MTU_DISCOVER, &flags, sizeof (flags));
+#endif
+	/*
+	 * Teredo multicast packets always have a TTL of 1.
+	 */
+	setsockopt (fd, SOL_IP, IP_MULTICAST_TTL, &flags, sizeof (flags));
+	return fd;
+}
+
+
+int teredo_send (int fd, const void *packet, size_t plen,
+                 uint32_t dest_ip, uint16_t dest_port)
+{
+	struct sockaddr_in nat_addr;
+
+	nat_addr.sin_family = AF_INET;
+	nat_addr.sin_port = dest_port;
+	nat_addr.sin_addr.s_addr = dest_ip;
+#ifdef HAVE_SA_LEN
+	nat_addr.sin_len = sizeof (nat_addr);
+#endif
+
+	return sendto (fd, packet, plen, 0, (struct sockaddr *)&nat_addr,
+			sizeof (nat_addr)) == (int)plen ? 0 : -1;
+}
