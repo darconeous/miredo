9a59e8317a0a82d87721234d0145ac32750ee20a
Rémi Denis-Courmont <rdenis@simphalempin.com>
2005-12-24 11:24:17 +0000

    Separate each tests
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@819 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/test/hmac.c
+++ libteredo/test/hmac.c
@@ -1,5 +1,5 @@
 /*
- * regress.c - Libteredo regression tests
+ * hmac.c - Libteredo HMAC tests
  * $Id$
  */
 
@@ -23,37 +23,68 @@
 # include <config.h>
 #endif
 
+#include <stdbool.h>
 #include <string.h>
+#include <stdio.h>
 
 #if HAVE_STDINT_H
 # include <stdint.h> /* Mac OS X needs that */
 #endif
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <unistd.h> /* sleep () */
 
-#include <libteredo/teredo.h>
+#include "relay.h"
+#include "security.h"
 
 int main (void)
 {
-	union teredo_addr a1, a2;
+	struct in6_addr src, dst;
+	uint8_t hmac[LIBTEREDO_HMAC_LEN];
 
-	/* REGRESSION TEST 1: t6cmp() */
-	memcpy (&a1, "\x3f\xfe\x83\x1f\x8a\xc3\x9d\xdd"
-	             "\x80\x00\xf2\x27\x75\x3c\x67\x74", 16);
+	puts ("Checking libteredo HMAC...");
+	puts ("That should take about 30 seconds...");
+	libteredo_preinit ();
+	libteredo_client_preinit ();
 
-	memcpy (&a2, "\x3f\xfe\x83\x1f\x8a\xc3\x9d\xdd"
-	             "\x80\x00\xf2\x27\x75\x3c\x67\x75", 16);
-	if (t6cmp (&a1, &a2) == 0)
-		return 1;
-	
-	memcpy (&a2, "\x3f\xfe\x83\x1f\x8a\xc3\x9d\xdd"
-	             "\x80\x00\xf2\x28\x75\x3c\x67\x74", 16);
-	if (t6cmp (&a1, &a2) == 0)
-		return 1;
+	memcpy (&src, "\x3f\xfe\x83\x1f\x8a\xc3\x9d\xdd"
+			"\x80\x00\xf2\x27\x75\x3c\x67\x74", 16);
+	memcpy (&dst, "\x20\x02\xc0\x00\x02\x42\x12\x42"
+			"\x13\x43\x14\x44\x15\x45\x16\x46", 16);
+	if (!GenerateHMAC (&src, &dst, hmac))
+		return -1;
+	if (!CompareHMAC (&src, &dst, hmac))
+		return -1;
 
-	memcpy (&a2, &a1, 16);
-	if (t6cmp (&a1, &a2) != 0)
-		return 1;
+	/* retry after first run */
+	if (!GenerateHMAC (&dst, &src, hmac))
+		return -1;
+	if (!CompareHMAC (&dst, &src, hmac))
+		return -1;
+	if (CompareHMAC (&src, &dst, hmac))
+		/* mixed addresses : should fail */
+		return -1;
+
+	hmac[4] ^= 0x40;
+	if (CompareHMAC (&dst, &src, hmac))
+		/* altered hash : should fail */
+		return -1;
+
+	hmac[4] ^= 0x40;
+	if (!CompareHMAC (&dst, &src, hmac))
+		return -1;
+
+	sleep (3);
+	if (!CompareHMAC (&dst, &src, hmac))
+		return -1;
+	/* should still be valid after 3 seconds */
+
+	sleep (28);
+	/* should no longer be valid after 31 seconds */
+	if (CompareHMAC (&dst, &src, hmac))
+		return -1;
+
+	libteredo_terminate ();
 
 	return 0;
 }
