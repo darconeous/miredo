d259e309d7bf6a094928f52e1aaf0bb633a08f6d
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-03-04 09:46:58 +0000

    Update provisional C API
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@990 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/relay.h
+++ libteredo/relay.h
@@ -49,6 +49,9 @@ int libteredo_tunnel_set_cone_flag (libteredo_tunnel *t, bool flag);
 int libteredo_tunnel_set_client_mode (libteredo_tunnel *t, const char *s1,
                                       const char *s2);
 
+void *libteredo_set_privdata (libteredo_tunnel *, void *);
+void *libteredo_get_privdata (const libteredo_tunnel *);
+
 typedef void (*libteredo_recv_cb) (libteredo_tunnel *, const void *, size_t);
 void libteredo_set_recv_callback (libteredo_tunnel *t, libteredo_recv_cb cb);
 int libteredo_send (libteredo_tunnel *t, const void *data, size_t n);
@@ -58,9 +61,9 @@ typedef void (*libteredo_icmpv6_cb) (libteredo_tunnel *, const void *, size_t,
 void libteredo_set_icmpv6_callback (libteredo_tunnel *t,
                                     libteredo_icmpv6_cb cb);
 
-typedef void (*libteredo_state_up_cb) (libteredo_tunnel *,
+typedef void (*libteredo_state_up_cb) (const libteredo_tunnel *,
                                        const struct in6_addr *, uint16_t);
-typedef void (*libteredo_state_down_cb) (libteredo_tunnel *);
+typedef void (*libteredo_state_down_cb) (const libteredo_tunnel *);
 void libteredo_set_state_cb (libteredo_tunnel *t, libteredo_state_up_cb u,
                              libteredo_state_down_cb d);
 
