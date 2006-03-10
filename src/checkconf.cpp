d7991ae76df6431774e3696905de720682cf0cbc
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-03-10 22:07:47 +0000

    Fix checkconf.cpp
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1015 6046fb1f-dae2-0310-8472-869a43925389

--- src/checkconf.cpp
+++ src/checkconf.cpp
@@ -73,6 +73,8 @@ int main(int argc, char *argv[])
 			if (asprintf (&str, "%s/../misc/miredo.conf-dist",
 			              srcdir) == -1)
 				filename = str = NULL;
+			else
+				filename = str;
 		}
 		else
 			filename = conffile;
