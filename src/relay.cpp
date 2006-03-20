726f5379c32bbeee0a248975d862028ecc3c3ba5
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-03-20 17:51:14 +0000

    libcap: add required CAP_KILL
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1056 6046fb1f-dae2-0310-8472-869a43925389

--- src/relay.cpp
+++ src/relay.cpp
@@ -74,8 +74,11 @@ const char *const miredo_pidfile = LOCALSTATEDIR"/run/miredo.pid";
 #ifdef HAVE_LIBCAP
 static const cap_value_t capv[] =
 {
-	CAP_SYS_CHROOT,
+	CAP_KILL, /* required by the signal handler */
 	CAP_SETUID,
+# ifdef MIREDO_CHROOT
+	CAP_SYS_CHROOT,
+# endif
 	CAP_NET_ADMIN, /* required by libtun6 */
 	CAP_NET_RAW /* required for raw ICMPv6 socket */
 };
