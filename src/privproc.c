bf46d9219793b92c7990f7e572751091fc0bca01
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-04-01 14:08:52 +0000

    Cosmetic fix
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1096 6046fb1f-dae2-0310-8472-869a43925389

--- src/privproc.c
+++ src/privproc.c
@@ -4,7 +4,7 @@
  */
 
 /***********************************************************************
- *  Copyright © 2004-2006 Rémi Denis-Courmont.                          *
+ *  Copyright © 2004-2006 Rémi Denis-Courmont.                         *
  *  This program is free software; you can redistribute and/or modify  *
  *  it under the terms of the GNU General Public License as published  *
  *  by the Free Software Foundation; version 2 of the license.         *
