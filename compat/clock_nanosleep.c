ed6e27740a1e3a2cb630656dba8d4568e7589470
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-05-25 14:39:35 +0000

    Replacements for clock_gettime() and clock_nanosleep()
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1443 6046fb1f-dae2-0310-8472-869a43925389

---
+++ compat/clock_nanosleep.c
@@ -0,0 +1,58 @@
+/*
+ * clock_nanosleep.c - clock_nanosleep() replacement
+ * $Id$
+ */
+
+/***********************************************************************
+ *  Copyright © 2006 Rémi Denis-Courmont.                              *
+ *  This program is free software; you can redistribute and/or modify  *
+ *  it under the terms of the GNU General Public License as published  *
+ *  by the Free Software Foundation; version 2 of the license.         *
+ *                                                                     *
+ *  This program is distributed in the hope that it will be useful,    *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               *
+ *  See the GNU General Public License for more details.               *
+ *                                                                     *
+ *  You should have received a copy of the GNU General Public License  *
+ *  along with this program; if not, you can get it from:              *
+ *  http://www.gnu.org/copyleft/gpl.html                               *
+ ***********************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <time.h>
+#include <errno.h>
+
+int clock_nanosleep (clockid_t id, int flags, const struct timespec *ts,
+                     struct timespec *ots)
+{
+	if (id != CLOCK_REALTIME)
+	{
+		errno = EINVAL;
+		return -1;
+	}
+
+	if (flags & TIMER_ABSTIME)
+	{
+		struct timespec mine;
+
+		if (clock_gettime (id, &mine))
+			return -1;
+
+		if (mine.tv_nsec < ts->tv_nsec)
+		{
+			mine.tv_nsec += 1000000000;
+			mine.tv_sec--;
+		}
+		mine.tv_sec -= ts->tv_sec;
+		mine.tv_nsec -= ts->tv_nsec;
+
+		/* With TIMER_ABSTIME, clock_nanosleep ignores <ots> */
+		return nanosleep (&mine, NULL);
+	}
+
+	return nanosleep (ts, ots);
+}
+
