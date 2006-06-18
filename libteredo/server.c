ca6a86028736d02db6c64e7acdeff79427f762ca
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-06-18 16:27:38 +0000

    Fix Router Solicitation sanity checks. This allows miredo-server to interact with Windows Vista.
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1519 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/server.c
+++ libteredo/server.c
@@ -330,7 +330,7 @@ teredo_process_packet (const teredo_server *s, bool sec)
 	if (IN6_IS_ADDR_LINKLOCAL (&ip6.ip6_src)
 	 && IN6_ARE_ADDR_EQUAL (&in6addr_allrouters, &ip6.ip6_dst)
 	 && (ip6.ip6_nxt == IPPROTO_ICMPV6)
-	 && (ip6len > sizeof (struct nd_router_solicit))
+	 && (ip6len >= sizeof (struct nd_router_solicit))
 	 && (((struct icmp6_hdr *)ptr)->icmp6_type == ND_ROUTER_SOLICIT))
 		// sends a Router Advertisement
 		return SendRA (s, &packet, &ip6.ip6_src, sec) ? 1 : -1;
