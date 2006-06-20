9410b7f4b7eb76fcbac3c528f08160f2b68a0c83
Rémi Denis-Courmont <rdenis@simphalempin.com>
2006-06-20 19:51:26 +0000

    Unit tests for IN6_* Teredo macros.
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1525 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/test/addrcmp.c
+++ libteredo/test/addrcmp.c
@@ -38,21 +38,30 @@ int main (void)
 	union teredo_addr a1, a2;
 
 	memcpy (&a1, "\x20\x01\x00\x00\xc0\x00\x02\x01"
-	             "\x80\x00\xf2\x27\x3f\xff\xfd\x74", 16);
+	             "\x80\x00\xcf\xc6\x3f\xff\xfd\x74", 16);
 
 	memcpy (&a2, "\x20\x01\x00\x00\xc0\x00\x02\x01"
-	             "\x80\x00\xf2\x27\x3f\xff\xfd\x75", 16);
+	             "\x80\x00\xcf\xc6\x3f\xff\xfd\x75", 16);
 	if (t6cmp (&a1, &a2) == 0)
 		return 1;
 	
 	memcpy (&a2, "\x20\x01\x00\x00\xc0\x00\x02\x01"
-	             "\x80\x00\xf2\x28\x3f\xff\xfd\x74", 16);
+	             "\x80\x00\xcf\xc7\x3f\xff\xfd\x74", 16);
 	if (t6cmp (&a1, &a2) == 0)
 		return 1;
 
 	memcpy (&a2, &a1, 16);
 	if (t6cmp (&a1, &a2) != 0)
 		return 1;
+
+	if ((IN6_TEREDO_PORT (&a1) != htons (12345))
+	 || (IN6_TEREDO_IPV4 (&a1) != htonl (0xc000028b))
+	 || (IN6_TEREDO_SERVER (&a1) != htonl (0xc0000201))
+	 || (!IN6_IS_TEREDO_ADDR_CONE (&a1))
+	 || (!IN6_MATCHES_TEREDO_CLIENT (&a1, htonl (0xc000028b), htons (12345)))
+	 || IN6_MATCHES_TEREDO_CLIENT (&a1, htonl (0xc000028b), htons (12346))
+	 || IN6_MATCHES_TEREDO_CLIENT (&a1, htonl (0xc000028c), htons (12345)))
+			return 1;
 
 	return 0;
 }
