6c50aca0be22dd7f07572b32b6938bc939d4b7f1
Rémi Denis-Courmont <rdenis@simphalempin.com>
2007-02-04 19:38:38 +0000

    Make the legacy checksumming interface a stub for the new one
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@1922 6046fb1f-dae2-0310-8472-869a43925389

--- libteredo/checksum.h
+++ libteredo/checksum.h
@@ -25,52 +25,15 @@
 # include <sys/types.h>
 # include <netinet/in.h>
 
-static inline uint16_t
-sum16 (const uint16_t *data, size_t length, uint32_t sum32)
-{
-	for (; length >= 2; length -= 2)
-		sum32 += *data++;
-
-	if (length) // trailing byte if length is odd
-		sum32 += ntohs((*(const uint8_t *)data) << 8);
-
-	while (sum32 > 0xffff)
-		sum32 = (sum32 & 0xffff) + (sum32 >> 16);
-	
-	return sum32;
-}
-
 /*
- * Computes an IPv6 Pseudo-header 16-bits checksum
- */
-static inline uint16_t 
-ipv6_sum (const struct ip6_hdr *ip6)
-{
-	uint32_t sum32 = 0;
-	size_t i;
-
-	/* Pseudo-header sum */
-	for (i = 0; i < 16; i += 2)
-		sum32 += *(const uint16_t *)(&ip6->ip6_src.s6_addr[i]);
-	for (i = 0; i < 16; i += 2)
-		sum32 += *(const uint16_t *)(&ip6->ip6_dst.s6_addr[i]);
-
-	sum32 += ip6->ip6_plen + ntohs (ip6->ip6_nxt);
-
-	while (sum32 > 0xffff)
-		sum32 = (sum32 & 0xffff) + (sum32 >> 16);
-
-	return sum32;
-}
-
-/*
- * Computes an ICMPv6 over IPv6 packet checksum
+ * Computes an ICMPv6 over IPv6 packet checksum.
+ * Jumbo datagrams not supported (but you don't care, do you?).
  */
 static inline uint16_t
 icmp6_checksum (const struct ip6_hdr *ip6, const struct icmp6_hdr *icmp6)
 {
-	return ~sum16 ((const uint16_t *)icmp6, ntohs (ip6->ip6_plen),
-			ipv6_sum (ip6));
+	struct iovec iov = { (void *)icmp6, ntohs (ip6->ip6_plen) };
+	return teredo_cksum (&ip6->ip6_src, &ip6->ip6_dst, IPPROTO_ICMPV6, &iov, 1);
 }
 
 #endif
