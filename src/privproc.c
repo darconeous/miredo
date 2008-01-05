19b4be55f1879469ecbb9d471b212d3145ffc993
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-01-05 14:43:28 +0000

    Clears the signal mask before running the callback script. Some programs could potentially be confused otherwise if they use signals.
    git-svn-id: https://svn.remlab.net/svn/miredo/trunk@2094 6046fb1f-dae2-0310-8472-869a43925389

--- src/privproc.c
+++ src/privproc.c
@@ -75,8 +75,14 @@ static int run_script (void)
 			return -1;
 
 		case 0:
+		{
+			sigset_t emptyset;
+			sigemptyset (&emptyset);
+			pthread_sigmask (SIG_SETMASK, &emptyset, NULL);
+
 			execl (script_path, script_path, (char *)NULL);
 			exit (1);
+		}
 	}
 
 	int res;
