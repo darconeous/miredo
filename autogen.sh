a38042b8dd188549d0a8b2bd5ca386ea4e4dc618
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-02-17 11:46:23 +0200

    Fix gettext-induced warnings

--- autogen.sh
+++ autogen.sh
@@ -37,7 +37,7 @@ mkdir -p include/
 for d in /usr /usr/local /opt/gettext /opt/local/share/gettext \
 		/usr/pkg "$HOME"; do
 	if test -f "$d/share/gettext/gettext.h" ; then
-		ln -sf "$d/share/gettext/gettext.h" include/gettext.h
+		cp -f -- "$d/share/gettext/gettext.h" include/gettext.h
 	fi
 done
 
@@ -48,6 +48,10 @@ echo "Error: can't find <gettext.h> convenience C header."
 echo "Please put a link to it by hand as include/gettext.h"
 exit 1
 }
+sed \
+	-e 's,!__STRICT_ANSI__,!defined(__STRICT_ANSI__),g' \
+	-e 's,if ENABLE_NLS,ifdef ENABLE_NLS,g' \
+	-i include/gettext.h
 
 echo ""
 echo "Type \`./configure' to configure the package for your system"
